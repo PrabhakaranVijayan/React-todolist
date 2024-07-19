4bffe4f5200431a05fb3c0b15f9b8cf2a7f96ee9
prabhakaran <prabhakarvijayan24@gmail.com>
2024-07-19 13:57:25 +0530

    todo-done

--- todolist/src/components/Todo.jsx
+++ todolist/src/components/Todo.jsx
@@ -3,7 +3,7 @@ import Todoitems from './Todoitems'
 
 const Todo = () => {
 
-  const[items,setitems]= useState([]);
+  const[items,setodos]= useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]);
     const inputvalue= useRef();
     const add=()=>{
       const value= inputvalue.current.value.trim()
@@ -16,24 +16,36 @@ const Todo = () => {
         task:value
         
       } 
-      setitems((prev)=>[...prev,newtask]) 
+      setodos((prev)=>[...prev,newtask]) 
       inputvalue.current.value="" 
     }
 
     const deletetask=(id)=>{
-      setitems((prevtodo)=>{
-        return prevtodo.filter((one)=>{one.id !== id})
+      setodos((prevtodo)=>{
+        return prevtodo.filter((one)=>{return one.id !== id})
 
-    })
-  }
+      })
+      
+    }
+    const toggle=(id)=>{
+      setodos((pre)=>{
+        return pre.map((each)=>{
+          if(each.id==id){
+            return {...each,iscompleted: !each.iscompleted}
+          }
+          return each
+        })
+      })
+    }
     useEffect(()=>{
       console.log(items)
+      localStorage.setItem("todos",JSON.stringify(items))
 
     },[items])
     
   return (
     
-    <div className='bg-white w-1/3 h-3/4 rounded-xl mx-auto'>
+    <div className='bg-white md:w-4/12 sm:w-10/12 w-10/12 sm:h-5/6 h-5/6 md:h-5/6 rounded-xl mx-auto'>
         {/* heading */}
         <div className='flex justify-center py-3'>
             <h1 className='font-sans font-semibold'>Todo-list</h1>
@@ -48,7 +60,7 @@ const Todo = () => {
         {/* todoitems */}
         <div>
           {items.map((item)=>{
-             return <Todoitems key={item.id} text={item.task} id={item.id} deletetodo={deletetask} iscomplete= {item.iscompleted} />
+             return <Todoitems toggle={toggle} key={item.id} text={item.task} id={item.id} deletetask={deletetask} iscomplete= {item.iscompleted} />
           })}
               
         </div>
--- todolist/src/components/Todoitems.jsx
+++ todolist/src/components/Todoitems.jsx
@@ -4,12 +4,12 @@ import circle from "../assets/radio-circle-regular-24.png"
 import del from "../assets/trash-solid-24.png"
 
 
-const Todoitems = ({text, id, deletetodo, iscomplete}) => {
+const Todoitems = ({text, id, deletetask, iscomplete,toggle}) => {
   return (
     <div className='flex items-center ps-2 pe-12 my-1'>
-        <img className='w-7' src={circle} alt='checkedimage' />
-        <p className='flex-auto ps-3 font-medium text-lg pb-1' style={{textDecoration: iscomplete?"line-through":"none"}}>{text}</p>
-        <img className='w-6' onClick={()=>{deletetodo(id)}} src={del} alt='deleteimage' />
+        <img className='w-7' src={iscomplete?checked:circle} alt='checkedimage' />
+        <p className={`flex-auto ps-3 font-medium text-lg pb-1} ${iscomplete?"line-through":"none"}`} onClick={()=>{toggle(id)}}>{text}</p>
+        <img className='w-6' onClick={()=>{deletetask(id)}} src={del} alt='deleteimage' />
     </div>
   )
 }
