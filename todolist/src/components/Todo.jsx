0595de4e5b56d7704218185fa91cf30b4336436c
prabhakaran <prabhakarvijayan24@gmail.com>
2024-07-19 11:01:23 +0530

    del-done

--- todolist/src/components/Todo.jsx
+++ todolist/src/components/Todo.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Todoitems from './Todoitems'
 
 const Todo = () => {
+
+  const[items,setitems]= useState([]);
+    const inputvalue= useRef();
+    const add=()=>{
+      const value= inputvalue.current.value.trim()
+      if(value.length===0){
+        return alert ("please add some task")
+      }
+      const newtask= {
+        id:Date.now(),
+        iscompleted:false,
+        task:value
+        
+      } 
+      setitems((prev)=>[...prev,newtask]) 
+      inputvalue.current.value="" 
+    }
+
+    const deletetask=(id)=>{
+      setitems((prevtodo)=>{
+        return prevtodo.filter((one)=>{one.id !== id})
+
+    })
+  }
+    useEffect(()=>{
+      console.log(items)
+
+    },[items])
+    
   return (
+    
     <div className='bg-white w-1/3 h-3/4 rounded-xl mx-auto'>
         {/* heading */}
         <div className='flex justify-center py-3'>
@@ -11,15 +41,16 @@ const Todo = () => {
         
         {/* input box */}
         <div className='flex justify-center my-3'>
-            <input className='border-0 outline-none focus:ring focus:border-blue-500 rounded-sm bg-slate-200 ps-2 me-1 py-1 w-2/3' placeholder='Add your task' type='text'/>
-            <button className='border-solid border-sky-800 rounded px-3 py-0 bg-sky-800 text-white'>Add</button>
+            <input ref={inputvalue} className='border-0 outline-none focus:ring focus:border-blue-500 rounded-sm bg-slate-200 ps-2 me-1 py-1 w-2/3' placeholder='Add your task' type='text'/>
+            <button onClick={add} className='border-solid border-sky-800 rounded px-3 py-0 bg-sky-800 text-white'>Add</button>
         </div>
 
         {/* todoitems */}
         <div>
-            <Todoitems />
-            <Todoitems item="learn html nad css" />
-            <Todoitems />
+          {items.map((item)=>{
+             return <Todoitems key={item.id} text={item.task} id={item.id} deletetodo={deletetask} iscomplete= {item.iscompleted} />
+          })}
+              
         </div>
 
 
--- todolist/src/components/Todoitems.jsx
+++ todolist/src/components/Todoitems.jsx
@@ -3,12 +3,13 @@ import checked from "../assets/check-circle-regular-24.png"
 import circle from "../assets/radio-circle-regular-24.png"
 import del from "../assets/trash-solid-24.png"
 
-const Todoitems = (props) => {
+
+const Todoitems = ({text, id, deletetodo, iscomplete}) => {
   return (
     <div className='flex items-center ps-2 pe-12 my-1'>
-        <img className='w-6' src={checked} alt='checkedimage' />
-        <p className='flex-auto ps-3'>{props.item}</p>
-        <img className='w-6' src={del} alt='deleteimage' />
+        <img className='w-7' src={circle} alt='checkedimage' />
+        <p className='flex-auto ps-3 font-medium text-lg pb-1' style={{textDecoration: iscomplete?"line-through":"none"}}>{text}</p>
+        <img className='w-6' onClick={()=>{deletetodo(id)}} src={del} alt='deleteimage' />
     </div>
   )
 }
